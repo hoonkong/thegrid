4bc3bda4813ae0a8ea6ca0123d54f3a0cc24703b
Hoon <hoonkong@gmail.com>
2013-10-22 00:28:33 -0700

    some movement

--- JS/block.js
+++ JS/block.js
@@ -7,7 +7,9 @@ var Block = function (ctx, blockSize, x, y, color)
     var color1 = color || "#FF0000";
     
     var id = "block" + x1 + "," + y1 + "," + color;
-    var position = x1 / blockSize;
+    var positionX = x1 / blockSize;
+    var positionY = y1 / blockSize;
+    var moveX = true;
     
     var draw = function ()
     {
@@ -17,8 +19,31 @@ var Block = function (ctx, blockSize, x, y, color)
     
     var update = function ()
     {
-        x1 = blockSize * position++;
-        console.log(id + ":" + position);
+        var direction = Math.random() * 10 / 2;
+        var dd = direction ? 1 : -1;
+        
+        if ((x1 + blockSize) >= 800 || x1 <= 0)
+        {
+            positionY = positionY * dd;
+            moveX = false;
+        }
+        else if ((y1 + blockSize) >= 500 || y1 <= 0)
+        {
+            positionX = positionX * dd;
+            moveX = true;
+        }
+       
+        if (moveX)
+        {
+            x1 = blockSize * positionX++;
+        }
+        else
+        {
+            y1 = blockSize * positionY++;
+        }
+        
+        
+        console.log(id + ":" + positionX + "," + positionY);
     }
     
     this.draw = draw;
